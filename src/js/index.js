28bf21319479ca0c0b621531eae567d6845c3659
전수경 <78544372+susoro@users.noreply.github.com>
2022-01-23 00:23:43 +0900

    7.메뉴추가하기

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,45 @@
-function App( ) {
+'use strict';
+
+
+// 가상함수를 사용해서 반복되는 작업을 단순화 시킨다.
+const $ = (selector) =>document.querySelector(selector);
+
+function App() {
+  // form태그가 자동으로 전송되는걸 막아준다.
+  $("#espresso-menu-form").addEventListener("submit", (e) => {
+    e.preventDefault(); //자동으로 전송되지 않는다
+  });
+
   // 메뉴의 이름을 입력받는것 
-  
-  }
+  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+    if(e.key === "Enter") {
+      const espressoMenuName = $("#espresso-menu-name").value;
+      const menuItemTemplate = (espressoMenuName) => {
+        return `
+        <li class="menu-list-item d-flex items-center py-2">
+        <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+        >
+          수정
+        </button>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+        >
+          삭제
+        </button>
+      </li>`;
+      };
+
+
+      $("#espresso-menu-list").insertAdjacentHTML(
+        "beforeend",
+        menuItemTemplate(espressoMenuName)
+      );
+    }
+  });
+}
+
+  App();
